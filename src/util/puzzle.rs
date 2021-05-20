02bd0d0867c0c8e3089aadf28ff92b67e756d42f
kashi <24215309+kashi03@users.noreply.github.com>
2021-05-20 23:39:53 +0900

    update: 必要なメソッドの仮実装

--- src/util/puzzle.rs
+++ src/util/puzzle.rs
@@ -1,5 +1,6 @@
-#[derive(Debug)]
-#[derive(Default)]
+use std::collections::HashSet;
+
+#[derive(Debug, Default)]
 pub struct Drop {
     pub p_type: i32,
     pub is_delete: bool,
@@ -22,5 +23,39 @@ impl Puzzle {
             }
             println!()
         }
+    }
+
+    fn next_field(&self) {
+
+    }
+
+    fn check_drop(&self, x:usize, y:usize, p_type:i32, hash:String) {
+
+    }
+
+    pub fn get_combo(&self) -> i32 {
+        let mut combo: i32 = 0;
+        loop {
+            for (y, row) in self.field.iter().enumerate() {
+                for (x, col) in row.iter().enumerate() {
+                    self.check_drop(x, y, col.p_type, format!("{}{}", x, y));
+                }
+            }
+
+            let mut combo_hash: HashSet<String> = HashSet::new();
+            for row in self.field.iter() {
+                for col in row.iter() {
+                    if col.combo_hash != "" {
+                        combo_hash.insert((*col.combo_hash).to_string());
+                    }
+                }
+            }
+            if combo_hash.len() == 0 {
+                break;
+            }
+            combo += combo_hash.len() as i32;
+            self.next_field();
+        }
+        combo
     }
 }
