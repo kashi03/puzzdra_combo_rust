4f9fc54cb97fbbed3502c24f429e5e1273922df0
kashi <24215309+kashi03@users.noreply.github.com>
2021-05-22 13:37:44 +0900

    update: コンボ解析の実装

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
 mod util;
 use util::puzzle as pz;
+use std::cell::{Cell, RefCell};
 
 fn main() {
     let f: [[i32;6];5] = [
-        [0,1,2,3,4,5],
+        [0,0,0,3,4,5],
         [1,2,3,4,5,0],
-        [2,3,4,5,0,1],
-        [3,4,5,0,1,2],
-        [4,5,0,1,2,3],
+        [2,3,3,5,0,1],
+        [2,2,2,0,1,2],
+        [2,5,3,1,2,3],
     ];
     let mut field: [[pz::Drop; 6]; 5] = Default::default();
     for (i, row) in f.iter().enumerate() {
         for (j, col) in row.iter().enumerate() {
             field[i][j] = pz::Drop {
-                p_type: *col,
-                is_delete: false,
-                is_search: false,
-                combo_hash: String::from(""),
+                drop_type: Cell::new(*col),
+                // is_delete: Cell::new(false),
+                is_search: Cell::new(false),
+                combo_hash: RefCell::new("".to_string()),
             }
         }
     }
@@ -26,5 +27,7 @@ fn main() {
         field_width: 6,
         field_height: 5,
     };
+    let combo = a.get_combo();
     a.show();
+    println!("{}", combo)
 }
--- src/util/puzzle.rs
+++ src/util/puzzle.rs
@@ -2,10 +2,10 @@ use std::collections::HashSet;
 
 #[derive(Debug, Default)]
 pub struct Drop {
-    pub p_type: i32,
-    pub is_delete: bool,
-    pub is_search: bool,
-    pub combo_hash: String,
+    pub drop_type: std::cell::Cell<i32>,
+    // pub is_delete: std::cell::Cell<bool>,
+    pub is_search: std::cell::Cell<bool>,
+    pub combo_hash: std::cell::RefCell<String>,
 }
 
 #[derive(Debug)]
@@ -19,18 +19,78 @@ impl Puzzle {
     pub fn show(&self) {
         for i in self.field.iter() {
             for j in i.iter() {
-                print!("{} ", j.p_type)
+                // print!("{} ", j.combo_hash.borrow().to_string())
+                print!("{:02} ", j.drop_type.get())
             }
             println!()
         }
     }
 
-    fn next_field(&self) {
-
+    fn down_drop(&self, x:usize, y:usize) {
+        if self.field[y+1][x].combo_hash.borrow().to_string() != "" {
+            let tmp1 = self.field[y][x].drop_type.get();
+            let tmp2 = self.field[y+1][x].drop_type.get();
+            self.field[y][x].drop_type.set(tmp2);
+            self.field[y+1][x].drop_type.set(tmp1);
+    
+            let tmp3 = self.field[y][x].is_search.get();
+            let tmp4 = self.field[y+1][x].is_search.get();
+            self.field[y][x].is_search.set(tmp4);
+            self.field[y+1][x].is_search.set(tmp3);
+    
+            let tmp5 = self.field[y][x].combo_hash.borrow().to_string();
+            let tmp6 = self.field[y+1][x].combo_hash.borrow().to_string();
+            self.field[y][x].combo_hash.replace(tmp6);
+            self.field[y+1][x].combo_hash.replace(tmp5);
+            if y as i32 +1 < self.field_height-1 {
+                self.down_drop(x, y+1)
+            }
+        }
     }
 
-    fn check_drop(&self, x:usize, y:usize, p_type:i32, hash:String) {
+    fn next_field(&self) {
+        for y in (0..self.field_height-2).rev() {
+            for x in (0..self.field_width-1).rev() {
+                self.down_drop(x as usize, y as usize);
+            }
+        }
+        for y in 0..self.field_height as usize {
+            for x in 0..self.field_width as usize {
+                if self.field[y][x].combo_hash.borrow().to_string() != "" {
+                    self.field[y][x].drop_type.set(-1);
+                }
+                self.field[y][x].is_search.set(false);
+                self.field[y][x].combo_hash.replace("".to_string());
+            }
+        }
+    }
 
+    fn check_drop(&self, x:usize, y:usize, drop_type:i32, hash:String) {
+        if drop_type != self.field[y][x].drop_type.get() || self.field[y][x].is_search.get() || self.field[y][x].drop_type.get() == -1 {
+            return
+        } else {
+            self.field[y][x].is_search.set(true);
+            let xx: i32 = x.clone() as i32;
+            let yy: i32 = y.clone() as i32;
+            if xx -1 >= 0 && xx +1 < self.field_width {
+                if self.field[y][x-1].drop_type.get()  == drop_type && self.field[y][x+1].drop_type.get() == drop_type {
+                    self.field[y][x-1].combo_hash.replace(hash.clone());
+                    self.field[y][x].combo_hash.replace(hash.clone());
+                    self.field[y][x+1].combo_hash.replace(hash.clone());
+                }
+            }
+            if yy -1 >= 0 && yy +1 < self.field_height {
+                if self.field[y-1][x].drop_type.get()  == drop_type && self.field[y+1][x].drop_type.get() == drop_type {
+                    self.field[y-1][x].combo_hash.replace(hash.clone());
+                    self.field[y][x].combo_hash.replace(hash.clone());
+                    self.field[y+1][x].combo_hash.replace(hash.clone());
+                }
+            }
+            if xx-1 >= 0 { self.check_drop(x-1, y, drop_type, hash.clone()) } // 左
+            if xx+1 < self.field_width { self.check_drop(x+1, y, drop_type, hash.clone()) } // 右
+            if yy-1 >= 0 { self.check_drop(x, y-1, drop_type, hash.clone()) } // 上
+            if yy+1 < self.field_height { self.check_drop(x, y+1, drop_type, hash.clone()) } // 下
+        }
     }
 
     pub fn get_combo(&self) -> i32 {
@@ -38,15 +98,15 @@ impl Puzzle {
         loop {
             for (y, row) in self.field.iter().enumerate() {
                 for (x, col) in row.iter().enumerate() {
-                    self.check_drop(x, y, col.p_type, format!("{}{}", x, y));
+                    self.check_drop(x, y, col.drop_type.get(), format!("{}{}", x, y));
                 }
             }
 
             let mut combo_hash: HashSet<String> = HashSet::new();
             for row in self.field.iter() {
                 for col in row.iter() {
-                    if col.combo_hash != "" {
-                        combo_hash.insert((*col.combo_hash).to_string());
+                    if col.combo_hash.borrow().to_string() != "" {
+                        combo_hash.insert(col.combo_hash.borrow().to_string());
                     }
                 }
             }
